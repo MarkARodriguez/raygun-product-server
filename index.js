24634f187156cd04fb048defd6ee9b310866a21a
Mark Rodriguez <markrodriguez@Marks-MacBook-Air.local>
2024-01-04 05:15:37 -0500

    fixed brand product data endpoint

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ console.log(databaseIds);
 const cors = require('cors');
 app.use(cors()); 
 
-// Function to execute the query and handle the results
+//grabbing the product data and writing a file
 async function executeQuery(databaseId) {
   try {
     const data = await queryNotionDatabase(databaseId);
-    console.log(data);
+    // console.log(data);
     // Save the data to a file
     fs.writeFile('productData.json', JSON.stringify(data, null, 2), (err) => {
       if (err) console.error('Error writing to file:', err);
@@ -28,20 +28,6 @@ executeQuery(databaseIds.BlazySusan);
 
 
 const port = 3001;
-// Reading the product data from the json file
-app.get('/products', (req, res) => {
-
-  fs.readFile('productData.json', 'utf8', (err, data) => {
-    console.log(data)
-    if (err) {
-      console.error(`Error reading file from disk: ${err}`);
-      res.status(500).send('Error reading data');
-    } else {
-      const jsonData = JSON.parse(data);
-      res.json(jsonData);
-    }
-  });
-});
 
 //endpoint for brands
 app.get('/brands', (req, res) => {
@@ -55,6 +41,44 @@ app.get('/brands', (req, res) => {
       res.json(jsonData);
     }
   });
+});
+
+//what i am working on now
+// Function to setup routes
+function sendBrandProducts(brands) {
+  brands.forEach(brand => {
+    app.get(`/brands/${brand.id}`, (req, res) => {
+      console.log(`Creating endpoint for brand with id: ${brand.id}`);
+      
+      // Read the associated brand data file
+      fs.readFile(`${brand.id}Data.json`, 'utf8', (err, data) => {
+        if (err) {
+          console.error(`Error reading file from disk: ${err}`);
+          res.status(500).send('Server Error');
+          return;
+        }
+
+        // Parse the data into a JavaScript object
+        const brandData = JSON.parse(data);
+        res.json(brandData);
+      });
+    });
+  });
+}
+
+// Read the brand data file
+fs.readFile('brandData.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Error reading file from disk: ${err}`);
+    return;
+  }
+
+  // Parse the data into a JavaScript object
+  const brands = JSON.parse(data);
+  console.log(`Successfully read file. Brands: ${JSON.stringify(brands)}`);
+
+  // Setup routes after file read operation has completed
+  sendBrandProducts(brands);
 });
 
 
@@ -72,25 +96,7 @@ app.get('/categories', (req, res) => {
   });
 });
 
-//payments
-// Node.js (Server-side) Example
-const stripe = require('stripe')('sk_test_51OTvQMLv1VTXKFwJpoj5ynGjePETsFGL4yF1u7OMrHjuUaDXNlS7TCmdemkd3q1l7Wv9xDnIx4WjBAX3Payqah7300WYTgvbt9');
-
-app.post('/create-payment-intent', async (req, res) => {
-  console.log('Received request for payment intent');
-
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      // amount: 10, // replace with the actual amount
-      currency: 'usd',
-      // additional parameters
-    });
-    res.send({clientSecret: paymentIntent.client_secret});
-  } catch (err) {
-    res.status(500).send({error: err.message});
-  }
-});
-
+ 
 
 // Listening to the port
 app.listen(port, () => {
