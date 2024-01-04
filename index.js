3be6ad5ac603c697f5bde4f8ef0c2b0cb7936070
Mark Rodriguez <markrodriguez@Marks-MacBook-Air.local>
2024-01-04 06:19:51 -0500

    committing in case

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const queryNotionDatabase = require('./notionQuery');
-const databaseIds = require('./databaseID.json');
 
 // Now you can access the IDs in your code
-console.log(databaseIds);
 const cors = require('cors');
 app.use(cors()); 
 
 //grabbing the product data and writing a file
-async function executeQuery(databaseId) {
+
+async function executeQuery(databaseId, brandId) {
   try {
     const data = await queryNotionDatabase(databaseId);
-    // console.log(data);
-    // Save the data to a file
-    fs.writeFile('productData.json', JSON.stringify(data, null, 2), (err) => {
+    // Save the data to a file named after the brand's id
+    fs.writeFile(`${brandId}Data.json`, JSON.stringify(data, null, 2), (err) => {
       if (err) console.error('Error writing to file:', err);
     });
   } catch (error) {
@@ -23,8 +21,25 @@ async function executeQuery(databaseId) {
   }
 }
 
-// Run the query initially
-executeQuery(databaseIds.BlazySusan);
+
+function processBrands(brands) {
+  brands.forEach(brand => {
+    // Execute the query for each brand using both databaseId and brand id
+    executeQuery(brand.databaseId, brand.id);
+  });
+}
+
+// Read the brand data file and process each brand
+fs.readFile('brandData.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Error reading file from disk: ${err}`);
+    return;
+  }
+
+  const brands = JSON.parse(data);
+  console.log(`Successfully read file. Processing brands...`);
+  processBrands(brands);
+});
 
 
 const port = 3001;
@@ -48,9 +63,9 @@ app.get('/brands', (req, res) => {
 function sendBrandProducts(brands) {
   brands.forEach(brand => {
     app.get(`/brands/${brand.id}`, (req, res) => {
-      console.log(`Creating endpoint for brand with id: ${brand.id}`);
+      console.log(`User requesting the following brand information: ${brand.id}`);
       
-      // Read the associated brand data file
+      // Read the associated brand data file using brand id
       fs.readFile(`${brand.id}Data.json`, 'utf8', (err, data) => {
         if (err) {
           console.error(`Error reading file from disk: ${err}`);
@@ -58,7 +73,6 @@ function sendBrandProducts(brands) {
           return;
         }
 
-        // Parse the data into a JavaScript object
         const brandData = JSON.parse(data);
         res.json(brandData);
       });
